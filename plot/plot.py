af1a06920a92f027458f92e8845bdc7b657346fe
akane-minami <akane03730913@gmail.com>
2022-09-14 14:22:59 +0900

    plot fixed

--- plot/plot.py
+++ plot/plot.py
@@ -5,10 +5,13 @@ import pandas as pd
 import numpy as np
 import matplotlib as mpl
 import matplotlib.pyplot as plt
+import matplotlib.ticker as ptick
 
+# python3 plot/plot.py csv/指定ディレクトリ
 
 """定数系"""
 n_steps = 100000
+name = ['Regional LinRS', 'LinTS', 'LinUCB', 'Greedy']
 
 """引数確認"""
 args = sys.argv
@@ -20,11 +23,12 @@ if len(args) <= 1:
 directory = os.listdir(args[1])
 files = [f for f in directory if os.path.isfile(os.path.join(args[1], f))]
 print(directory)
-print(files)#時間が古い順にソートした方が良い
 policy_names = [file_name[:-4].replace('_', ' ') for file_name in files]
+f = [files[policy_names.index(i)] for i in name]
+policy_names = [file_name[:-4].replace('_', ' ') for file_name in f]
 
 result_list = []
-for file_name in files:
+for file_name in f:
     df = pd.read_csv(args[1] + '/' + file_name)
     dict_type = df.to_dict(orient='list')
     result_list.append(dict_type)
@@ -39,8 +43,9 @@ results_dir = 'png/{0:%Y%m%d%H%M}/'.format(time_now)
 os.makedirs(results_dir, exist_ok=True)
 
 for i, data_name in enumerate(
-        ['rewards', 'regrets', 'accuracy', 'greedy_rate', 'errors']):
+        ['rewards', 'regrets', 'accuracy', 'greedy_rate', 'errors', 'entropy_of_reliability']):
     fig = plt.figure(figsize=(12, 8))
+    #fig = plt.figure(figsize=(8, 8))
     ax = fig.add_subplot(111)
 
     for j, policy_name in enumerate(policy_names):
@@ -54,43 +59,90 @@ for i, data_name in enumerate(
                              mode='same')
             # ax.plot(np.linspace(1, self.n_steps, num=self.n_steps), y3, label=policy_name+"moving_average", color=cmap(j), linewidth=1.5, alpha=0.8)
             ax.plot(np.linspace(1, n_steps, num=n_steps), y3,
-                    label=policy_name, color=cmap(j), linewidth=1.5,
+                    label=policy_name, color=cmap(j), linewidth=3,
                     alpha=0.8)
             ax.set_ylim([0.2, 1.1])
-        elif data_name == 'errors' or data_name == 'errors_greedy':
+        elif data_name == 'errors':
             """通常ver"""
             # ax.plot(np.linspace(1, self.n_steps, num=self.n_steps), self.result_list.at[j, data_name], label=policy_name, linewidth=1.5, alpha=0.8)
             # ax.fill_between(x=np.linspace(1, self.n_steps, num=self.n_steps), y1=self.result_list.at[j, 'min_'+data_name], y2=self.result_list.at[j, 'max_'+data_name], alpha=0.1)
             """移動平均ver"""
-            b = np.ones(10) / 10.0
+            b = np.ones(30) / 30.0
             y3 = np.convolve(result_list.at[j, data_name], b,
                              mode='same')  # 移動平均
             ax.plot(np.linspace(1, n_steps, num=n_steps), y3,
-                    label=policy_name + "moving_average",
-                    color=cmap(j), linewidth=1.5, alpha=0.8)
-            ax.set_ylim([0, 20.0])
+                    label=policy_name,
+                    color=cmap(j), linewidth=3, alpha=0.8)
+            ax.set_ylim([-5.0, 5.0])
+        elif data_name == 'entropy_of_reliability':
+            if 'LinRS' in policy_name:
+                ax.plot(np.linspace(1, n_steps, num=n_steps), result_list.at[j, data_name], label=policy_name, linewidth=3, alpha=0.8)
+                ax.set_ylim([0,1.1])
+
+        elif data_name == 'regrets':
+            """ 白黒plotの時
+            if policy_name == 'Regional LinRS':
+                    ax.plot(np.linspace(1, n_steps, num=n_steps),
+                    result_list.at[j, data_name],
+                    label=policy_name, color="k",linewidth=3, alpha=0.8)
+            elif policy_name == 'LinTS':
+                    ax.plot(np.linspace(1, n_steps, num=n_steps),
+                    result_list.at[j, data_name],
+                    label=policy_name, color="k",linestyle = "dashed",linewidth=3, alpha=0.8)  
+            elif policy_name == 'LinUCB':
+                    ax.plot(np.linspace(1, n_steps, num=n_steps),
+                    result_list.at[j, data_name],
+                    label=policy_name, color="k",linestyle = "dashdot",linewidth=3, alpha=0.8)  
+            elif policy_name == 'Greedy':
+                    ax.plot(np.linspace(1, n_steps, num=n_steps),
+                    result_list.at[j, data_name],
+                    label=policy_name, color="k",linestyle = "dotted",linewidth=3, alpha=0.8)"""
+            ax.plot(np.linspace(1, n_steps, num=n_steps),
+                    result_list.at[j, data_name],
+                    label=policy_name, linewidth=3, alpha=0.8)
+            ax.set_ylim([0,4000])
+        elif data_name == 'rewards':
+                ax.plot(np.linspace(1, n_steps, num=n_steps),
+                result_list.at[j, data_name],
+                label=policy_name, linewidth=3, alpha=0.8)
+                ax.set_ylim([0,80000])
         else:
             ax.plot(np.linspace(1, n_steps, num=n_steps),
                     result_list.at[j, data_name],
-                    label=policy_name, linewidth=1.5, alpha=0.8)
+                    label=policy_name, linewidth=3, alpha=0.8)
             # ax.fill_between(x=np.linspace(1, self.n_steps, num=self.n_steps), y1=self.result_list.at[j, 'min_'+data_name], y2=self.result_list.at[j, 'max_'+data_name], alpha=0.1)
-    ax.set_xlabel('steps', fontsize=14)
-    ax.set_ylabel(data_name, fontsize=14)
+
+    ax.set_xlabel('step', fontsize=23)
+    ax.xaxis.offsetText.set_fontsize(23)
+    #ax.ticklabel_format(style="sci", axis="x", scilimits=(4,4))   # 10^3単位の指数で表示
+    if data_name == 'rewards':
+        ax.set_ylabel('reward',fontsize=23)
+        ax.yaxis.offsetText.set_fontsize(23)
+        ax.ticklabel_format(style="sci", axis="y", scilimits=(4,4))   # 10^3単位の指数で表示
+    elif data_name == 'regrets':
+        ax.set_ylabel('regret',fontsize=23)
+    elif data_name == 'greedy_rate':
+        ax.set_ylabel('greedy rate',fontsize=23)
+    elif data_name == 'errors':
+        ax.set_ylabel('mean percentage error',fontsize=23)
+    elif data_name == 'entropy_of_reliability':
+        ax.set_ylabel('entropy of reliability',fontsize=23)
+    else:
+        ax.set_ylabel(data_name,fontsize=23)
+    #ax.spines["top"].set_linewidth(2)
+    #ax.spines["left"].set_linewidth(2)
+    #ax.spines["bottom"].set_linewidth(2)
+    #ax.spines["right"].set_linewidth(2)
     if data_name == 'greedy_rate' or data_name == 'accuracy':
+        leg = ax.legend(loc='lower right', fontsize=23)
+    if data_name == 'errors':
         leg = ax.legend(loc='lower right', fontsize=23)
     else:
         leg = ax.legend(loc='upper left', fontsize=23)
-    # leg.get_lines()[0].set_linewidth(3)
-    # leg.get_lines()[1].set_linewidth(3)
-    # leg.get_lines()[2].set_linewidth(3)
-    # leg.get_lines()[3].set_linewidth(3)
 
-    plt.tick_params(labelsize=10)
-    ax.grid(axis='y')
+    plt.tick_params(labelsize=23)
+    #ax.grid(alpha=0.8,color = "gray", linestyle="--")
 
-    # plt.show() # サーバで実行する際にエラーになるのでコメントアウト
-    # path = os.getcwd()#現在地
-    # results_dir = os.path.join(path, 'png/{0:%Y%m%d%H%M}/'.format(time_now))#保存場所
     fig.savefig(results_dir + data_name, bbox_inches='tight',
                 pad_inches=0)
 
--- plot/plot_survival_rate.py
+++ plot/plot_survival_rate.py
@@ -11,21 +11,17 @@ def mean_result(df):
     """1日単位で合計、sim数で平均"""
     df_result = pd.DataFrame()
     len_df = len(df.columns)
-    n = len_df//day if len_df%day == 0 else (len_df//day) + 1
+    n = len_df//day #あまり分は切り捨て
     for i in range(n):
         df_day = df.iloc[:, 0+day*i:day*(i+1)]
         df_day_reward = df_day.sum(axis=1)
         df_result=pd.concat([df_result, df_day_reward], axis=1)
 
-        #print(df_day_reward)
-        #print(df_result)
     #合計から生存したかいなか出す
     survival = np.where(df_result >= survival_rate, 1, 0)
     #sim数で平均。独立の生存率
     print(df_result)
-    #print(survival)
     survival_result_tmp = survival.mean(axis=0)
-    #print(survival_result_tmp)
     #本来の生存率を出す(前の生存率にかける)
     survival_result = np.zeros(len(survival_result_tmp))
     survival_result[0] = survival_result_tmp[0]
@@ -33,13 +29,13 @@ def mean_result(df):
     for j in range(len(survival_result_tmp)-1):
         survival_result[j+1] = survival_result[j] * survival_result_tmp[j+1]
 
-    #print(survival_result)
 
     return survival_result, n
         
 
 """定数系"""
 n_steps = 100000
+name = ['Regional LinRS', 'LinTS', 'LinUCB', 'Greedy']#ここを書き換える
 
 """引数確認"""
 args = sys.argv
@@ -57,11 +53,12 @@ day = int(args[3])
 directory = os.listdir(args[1])
 files = [f for f in directory if os.path.isfile(os.path.join(args[1], f))]
 print(directory)
-print(files)#時間が古い順にソートした方が良い
 policy_names = [file_name[:-4].replace('_', ' ') for file_name in files]
+f = [files[policy_names.index(i)] for i in name]
+policy_names = [file_name[:-4].replace('_', ' ') for file_name in f]
 
 result_list = []
-for file_name in files:
+for file_name in f:
     df = pd.read_csv(args[1] + '/' + file_name, index_col=0)
     df, n = mean_result(df)
     df = df.tolist()
@@ -69,9 +66,6 @@ for file_name in files:
     result_list.append(df)
 #result_list = pd.DataFrame(result_list)
 print(result_list)#生存率
-
-#ここからしたやること
-#シンプルにそれをplot、つまり下より簡単に
 
 """結果データのプロット"""
 mpl.rcParams['axes.xmargin'] = 0
@@ -81,20 +75,20 @@ time_now = datetime.datetime.now()
 results_dir = 'png/survival_rate/{0:%Y%m%d%H%M}/'.format(time_now)
 os.makedirs(results_dir, exist_ok=True)
 
-fig = plt.figure(figsize=(12, 8))
+fig = plt.figure(figsize=(8, 8))
 ax = fig.add_subplot(111)
 for j, policy_name in enumerate(policy_names):
     cmap = plt.get_cmap("tab10")
     ax.plot(np.linspace(1, n, num=n),
                     result_list[j],
-                    label=policy_name, linewidth=1.5, alpha=0.8)
-ax.set_ylim([0,1.1])
+                    label=policy_name, linewidth=3, alpha=0.8)
+ax.set_ylim([-0.05,1.05])
 
-ax.set_xlabel('day', fontsize=14)
-ax.set_ylabel('survival rate', fontsize=14)
-leg = ax.legend(loc='upper left', fontsize=23)
-plt.tick_params(labelsize=10)
-ax.grid(axis='y')
+ax.set_xlabel('day', fontsize=23)
+ax.set_ylabel('survival rate', fontsize=23)
+leg = ax.legend(loc='lower right', fontsize=23)
+plt.tick_params(labelsize=23)
+ax.grid(alpha=0.8,color = "gray", linestyle="--")
 
 fig.savefig(results_dir + 'survival_rate', bbox_inches='tight',
                 pad_inches=0)
