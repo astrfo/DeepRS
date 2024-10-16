14c53d3f8c4e795231bf92d9c263a8d1de1b5c6f
Jun <kumejun2000@gmail.com>
2024-10-16 19:14:56 +0900

    discrete_stateの廃止

--- agent.py
+++ agent.py
@@ -5,12 +5,12 @@ class Agent:
     def reset(self):
         self.policy.reset()
 
-    def action(self, state, discrete_state):
-        action = self.policy.action(state, discrete_state)
+    def action(self, state):
+        action = self.policy.action(state)
         return action
 
-    def greedy_action(self, state, discrete_state):
-        action = self.policy.greedy_action(state, discrete_state)
+    def greedy_action(self, state):
+        action = self.policy.greedy_action(state)
         return action
 
     def update(self, state, action, reward, next_state, done):
