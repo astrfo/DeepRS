b50dba2069ef5c95c23eea1bdcf75fd052108bd7
Jun <kumejun2000@gmail.com>
2024-10-19 10:08:56 +0900

    get_screenをutilsに移動

---
+++ utils/get_screen_utils.py
@@ -0,0 +1,12 @@
+import numpy as np
+import torchvision.transforms as T
+
+
+def get_screen(env):
+    resize = T.Compose([T.ToPILImage(),
+                    T.Resize(size=(84, 84)),
+                    T.Grayscale(num_output_channels=1)])
+    screen = resize(env.render())
+    screen = np.expand_dims(np.asarray(screen), axis=2).transpose((2, 0, 1))
+    screen = np.ascontiguousarray(screen, dtype=np.float64) / 255
+    return screen
