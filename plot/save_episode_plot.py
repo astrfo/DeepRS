5071a82c1a22df8bda23a2a05b0ae91f81affed9
Jun <kumejun2000@gmail.com>
2024-10-20 01:48:39 +0900

    save_step_plotの廃止、中身をsave_episode_plotへ移動

--- plot/save_episode_plot.py
+++ plot/save_episode_plot.py
@@ -18,3 +18,23 @@ def save_episode_plot(collector, sim_dir_path):
     plt.legend()
     plt.savefig(sim_dir_path + 'survived_step.png')
     plt.close()
+
+    plt.figure(figsize=(12, 8))
+    plt.plot(collector.q_value_step_list, label='Q_value')
+    plt.title('Q_value per Step')
+    plt.xlabel('Step')
+    plt.xlim(-1, len(collector.q_value_step_list) + 1)
+    plt.legend()
+    plt.savefig(sim_dir_path + 'q_value.png')
+    plt.close()
+    if not collector.is_aleph_s_in_policy:
+        return
+    plt.figure(figsize=(12, 8))
+    plt.plot(collector.q_value_step_list, label='Q_value')
+    plt.plot(collector.aleph_step_list, label='Aleph')
+    plt.title('Q_value and Aleph per Step')
+    plt.xlabel('Step')
+    plt.xlim(-1, len(collector.aleph_step_list) + 1)
+    plt.legend()
+    plt.savefig(sim_dir_path + 'q_value_and_aleph.png')
+    plt.close()
