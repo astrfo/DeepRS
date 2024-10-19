1d12df980988ca8dea47d6653e0b0a5a79ed1005
Jun <kumejun2000@gmail.com>
2024-10-19 18:37:56 +0900

    aleph_GとE_Gを削除

--- policy/conv_rsrsaleph_dqn.py
+++ policy/conv_rsrsaleph_dqn.py
@@ -39,14 +39,6 @@ class ConvRSRSAlephDQN(nn.Module):
         self.optimizer = optim.Adam(self.model.parameters(), lr=self.alpha)
         self.criterion = nn.MSELoss(reduction='sum')
         self.n = np.zeros(self.action_space)
-        self.total_step = 0
-        self.gamma_G = 0.9
-        self.aleph_G = kwargs.get('aleph_G', 1.0)
-        self.E_G = 0
-        # self.zeta = 1
-        self.q_list = [[] for _ in range(self.state_space)]
-        self.E_G_list = []
-        self.aleph_G_list = []
 
     def reset(self):
         self.replay_buffer.reset()
@@ -57,11 +49,6 @@ class ConvRSRSAlephDQN(nn.Module):
         self.model_target.to(self.device)
         self.optimizer = optim.Adam(self.model.parameters(), lr=self.alpha)
         self.n = np.zeros(self.action_space)
-        self.total_step = 0
-        self.E_G = 0
-        self.q_list = [[] for _ in range(self.state_space)]
-        self.E_G_list = []
-        self.aleph_G_list = []
 
     def q_value(self, state):
         s = torch.tensor(state, dtype=torch.float64).to(self.device).unsqueeze(0)
