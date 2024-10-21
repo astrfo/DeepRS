6a2be6043b88290bf8dcd6a8040f24f8c487b238
Jun <kumejun2000@gmail.com>
2024-10-21 16:51:57 +0900

    sim累計をplotするようになるバグを修正

--- plot/save_episode_plot.py
+++ plot/save_episode_plot.py
@@ -2,19 +2,19 @@ import matplotlib.pyplot as plt
 
 def save_episode_plot(collector, sim_dir_path):
     plt.figure(figsize=(12, 8))
-    plt.plot(collector.reward_sim_list, label='Reward')
+    plt.plot(collector.reward_epi_list, label='Reward')
     plt.title('Reward per Episode')
     plt.xlabel('Episode')
-    plt.xlim(-1, len(collector.reward_sim_list) + 1)
+    plt.xlim(-1, len(collector.reward_epi_list) + 1)
     plt.legend()
     plt.savefig(sim_dir_path + 'reward.png')
     plt.close()
 
     plt.figure(figsize=(12, 8))
-    plt.plot(collector.survived_step_sim_list, label='Survived Step')
+    plt.plot(collector.survived_step_epi_list, label='Survived Step')
     plt.title('Survived Step per Episode')
     plt.xlabel('Episode')
-    plt.xlim(-1, len(collector.survived_step_sim_list) + 1)
+    plt.xlim(-1, len(collector.survived_step_epi_list) + 1)
     plt.legend()
     plt.savefig(sim_dir_path + 'survived_step.png')
     plt.close()
