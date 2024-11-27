8834bb469921260740237488d1592900652d289c
Jun <kumejun2000@gmail.com>
2024-11-28 02:17:15 +0900

    embedding_sizeを追加

--- network/rsrsnet.py
+++ network/rsrsnet.py
@@ -3,11 +3,11 @@ import torch.nn.functional as F
 
 
 class RSRSNet(nn.Module):
-    def __init__(self, input_size, hidden_size, output_size):
+    def __init__(self, input_size, hidden_size, embedding_size, output_size):
         super().__init__()
         self.fc1 = nn.Linear(input_size, hidden_size)
-        self.embed = nn.Linear(hidden_size, hidden_size)
-        self.fc2 = nn.Linear(hidden_size, output_size)
+        self.embed = nn.Linear(hidden_size, embedding_size)
+        self.fc2 = nn.Linear(embedding_size, output_size)
 
     def forward(self, x):
         x = F.relu(self.fc1(x))
