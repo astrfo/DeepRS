d62497f79f5654c25c6c0dc70f0f1835192412f0
Jun <kumejun2000@gmail.com>
2024-11-28 02:12:29 +0900

    ex_param_make_folderを削除 (windowsの文字数上限に引っかかるため)

--- utils/make_param_file_utils.py
+++ utils/make_param_file_utils.py
@@ -81,54 +81,6 @@ def compare_base_make_folder(env_name, algo, ex_param):
         exit(1)
     return results_dir
 
-
-def ex_param_make_folder(env_name, algo, ex_param):
-    if algo == 'DQN' or algo == 'DDQN' or algo == 'DuelingDQN' or algo == 'DuelingDDQN' or algo == 'DQN_RND':
-        use_param = ['sim', 'epi', 'alpha', 'gamma', 'epsilon', 'tau', 'hidden_size', 'replay_buffer_capacity', 'batch_size']
-        ex_folder_path = f'log/{env_name}/{algo}/'
-        os.makedirs(ex_folder_path, exist_ok=True)
-        folder_name = algo
-        for k, v in ex_param.items():
-            if k in use_param:
-                folder_name += f'_{k}{v}'
-        results_dir = f'{ex_folder_path}{folder_name}/'
-        os.makedirs(results_dir, exist_ok=True)
-    elif algo == 'ConvDQN' or algo == 'ConvDDQN' or algo == 'ConvDQN_RND':
-        use_param = ['sim', 'epi', 'alpha', 'gamma', 'epsilon', 'tau', 'hidden_size', 'replay_buffer_capacity', 'batch_size', 'neighbor_frames']
-        ex_folder_path = f'log/{env_name}/{algo}/'
-        os.makedirs(ex_folder_path, exist_ok=True)
-        folder_name = algo
-        for k, v in ex_param.items():
-            if k in use_param:
-                folder_name += f'_{k}{v}'
-        results_dir = f'{ex_folder_path}{folder_name}/'
-        os.makedirs(results_dir, exist_ok=True)
-    elif algo == 'ConvDQNAtari':
-        use_param = ['sim', 'epi', 'alpha', 'gamma', 'epsilon', 'epsilon_start', 'epsilon_end', 'epsilon_decay', 'learning_rate', 'target_update_freq', 'tau', 'hidden_size', 'replay_buffer_capacity', 'batch_size', 'warmup']
-        ex_folder_path = f'log/{env_name}/{algo}/'
-        os.makedirs(ex_folder_path, exist_ok=True)
-        folder_name = algo
-        for k, v in ex_param.items():
-            if k in use_param:
-                folder_name += f'_{k}{v}'
-        results_dir = f'{ex_folder_path}{folder_name}/'
-        os.makedirs(results_dir, exist_ok=True)
-    elif algo == 'RSRSDQN' or algo == 'RSRSDDQN' or algo == 'RSRSDuelingDQN' or algo == 'RSRSDuelingDDQN' or algo == 'RSRSAlephDQN' or algo == 'RSRSAlephQEpsDQN' or algo == 'RSRSAlephQEpsRASDQN' or algo == 'RSRSAlephQEpsRASChoiceDQN' or algo == 'RSRSAlephQEpsRASChoiceDQN_RND' or algo == 'ConvRSRSDQN' or algo == 'ConvRSRSDynDQN' or algo == 'ConvRSRSAlephDQN' or algo == 'ConvRSRSAlephQEpsRASChoiceDQN_RND' or algo == 'ConvRSRSAlephQEpsRASChoiceDQNAtari' or algo == 'ConvRSRSAlephQEpsRASChoiceCentroidDQNAtari':
-        use_param = ['sim', 'epi', 'alpha', 'gamma', 'epsilon_dash', 'tau', 'hidden_size', 'replay_buffer_capacity', 'episodic_memory_capacity', 'batch_size', 'neighbor_frames', 'warmup', 'k', 'zeta', 'aleph_G']
-        ex_folder_path = f'log/{env_name}/{algo}/'
-        os.makedirs(ex_folder_path, exist_ok=True)
-        folder_name = algo
-        for k, v in ex_param.items():
-            if k in use_param:
-                folder_name += f'_{k}{v}'
-        results_dir = f'{ex_folder_path}{folder_name}/'
-        os.makedirs(results_dir, exist_ok=True)
-    else:
-        print(f'Cannot make file for algorithm {algo}')
-        exit(1)
-    return results_dir
-
-
 def make_param_file(env_name, algo, param, model, policy, agent):
     result_dir_path = ex_param_make_folder(env_name, algo, param)
     f = open(result_dir_path + 'hyperparameter_list.txt', mode='w', encoding='utf-8')
