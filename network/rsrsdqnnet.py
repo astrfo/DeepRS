43fd7e801b991c306cb59e06685e61ccf3875ee6
Jun <kumejun2000@gmail.com>
2025-01-10 00:53:11 +0900

    RSRSDQNNetのネットワーク構造をDQNと同じにする

--- network/rsrsdqnnet.py
+++ network/rsrsdqnnet.py
@@ -6,13 +6,16 @@ class RSRSDQNNet(nn.Module):
     def __init__(self, input_size, hidden_size, output_size):
         super().__init__()
         self.fc1 = nn.Linear(input_size, hidden_size)
-        self.fc2 = nn.Linear(hidden_size, output_size)
+        self.fc2 = nn.Linear(hidden_size, hidden_size)
+        self.fc3 = nn.Linear(hidden_size, output_size)
 
     def forward(self, x):
         x = F.relu(self.fc1(x))
-        x = self.fc2(x)
+        x = F.relu(self.fc2(x))
+        x = self.fc3(x)
         return x
 
     def embedding(self, x):
-        x = self.fc1(x)
+        x = F.relu(self.fc1(x))
+        x = self.fc2(x)
         return x
