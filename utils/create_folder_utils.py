a2d774e46727ca688ccc5b155dd2f14f2226002d
Jun <kumejun2000@gmail.com>
2025-01-09 17:09:44 +0900

    フォルダ作成用utilsを追加

---
+++ utils/create_folder_utils.py
@@ -0,0 +1,19 @@
+import os
+
+
+def create_param_folder(env_name, folder_name):
+    result_dir_path = f'log/{env_name}/{folder_name}/'
+    os.makedirs(result_dir_path, exist_ok=True)
+    return result_dir_path
+
+
+def create_sim_folder(result_dir_path, sim):
+    sim_dir_path = result_dir_path + f'{sim}/'
+    os.makedirs(sim_dir_path, exist_ok=True)
+    return sim_dir_path
+
+
+def create_average_folder(result_dir_path):
+    average_sim_dir_path = result_dir_path + 'average/'
+    os.makedirs(average_sim_dir_path, exist_ok=True)
+    return average_sim_dir_path
