8084a6ea4c9a9b7fa9faa581937674646b7e3fe1
Jun <kumejun2000@gmail.com>
2025-01-09 16:13:59 +0900

    aleph_stateがあるかないかで管理されていたmetricsを削除，一旦綺麗に

--- collector.py
+++ collector.py
@@ -15,7 +15,6 @@ class Collector:
         self.agent = agent
         self.policy = policy
         self.sma_window = sma_window
-        self.is_aleph_state_in_policy = hasattr(self.policy, 'update_global_value')
 
         # step data
         self.reward_step_list = []
@@ -23,10 +22,6 @@ class Collector:
         self.q_value_step_list = []
         self.loss_step_list = []
         self.loss_sma_step_list = []
-        if self.is_aleph_state_in_policy:
-            self.aleph_state_step_list = []
-            self.aleph_beta_step_list = []
-            self.satisfy_unsatisfy_count_list = np.zeros(2)
 
         # episode data
         self.reward_epi_list = []
@@ -44,10 +39,6 @@ class Collector:
         data['q_value'] = self.q_value_step_list
         data['loss'] = self.loss_step_list
         data['loss_sma'] = self.loss_sma_step_list
-        if self.is_aleph_state_in_policy:
-            data['aleph'] = self.aleph_state_step_list
-            data['aleph_beta'] = self.aleph_beta_step_list
-            data['satisfy_unsatisfy_count'] = self.satisfy_unsatisfy_count_list
         return data
 
     def initialize(self):
@@ -68,17 +59,10 @@ class Collector:
     def collect_step_data(self, reward, survived_step):
         self.reward_step_list.append(reward)
         self.survived_step_step_list.append(survived_step)
-        q_value = self.agent.policy.q_value(self.agent.current_state)
+        q_value = self.policy.q_value(self.agent.current_state)
         self.q_value_step_list.append(q_value)
-        if self.policy.loss is not None:
-            self.loss_step_list.append(self.policy.loss.item())
-            self.loss_sma_step_list.append(self.calculate_sma(self.loss_step_list))
-        if self.is_aleph_state_in_policy and self.agent.policy.aleph_state is not None:
-            aleph = self.agent.policy.aleph_state
-            self.aleph_state_step_list.append(aleph)
-            self.aleph_beta_step_list.append(self.agent.policy.aleph_beta)
-            if max(q_value) >= aleph: self.satisfy_unsatisfy_count_list[0] += 1
-            else: self.satisfy_unsatisfy_count_list[1] += 1
+        self.loss_step_list.append(self.policy.loss.item())
+        self.loss_sma_step_list.append(self.calculate_sma(self.loss_step_list))
 
     def collect_episode_data(self, total_reward, survived_step):
         self.reward_epi_list.append(total_reward)
@@ -100,10 +84,6 @@ class Collector:
         np.savetxt(sim_dir_path + f'survived_step_sma_epi{epi}.csv', self.survived_step_sma_epi_list, delimiter=',')
         np.savetxt(sim_dir_path + f'loss_epi{epi}.csv', self.loss_step_list, delimiter=',')
         np.savetxt(sim_dir_path + f'loss_sma_epi{epi}.csv', self.loss_sma_step_list, delimiter=',')
-        if self.is_aleph_state_in_policy:
-            np.savetxt(sim_dir_path + f'aleph_state_epi{epi}.csv', self.aleph_state_step_list, delimiter=',')
-            np.savetxt(sim_dir_path + f'aleph_beta_epi{epi}.csv', self.aleph_beta_step_list, delimiter=',')
-            np.savetxt(sim_dir_path + f'satisfy_unsatisfy_count_epi{epi}.csv', self.satisfy_unsatisfy_count_list, delimiter=',')
         
         episode_data = self.format()
         with open(sim_dir_path + f'episode{epi}_{uuid.uuid4().hex[:6]}.pickle', 'wb') as f:
@@ -118,10 +98,6 @@ class Collector:
         np.savetxt(sim_dir_path + 'q_value.csv', self.q_value_step_list, delimiter=',')
         np.savetxt(sim_dir_path + 'loss.csv', self.loss_step_list, delimiter=',')
         np.savetxt(sim_dir_path + 'loss_sma.csv', self.loss_sma_step_list, delimiter=',')
-        if self.is_aleph_state_in_policy:
-            np.savetxt(sim_dir_path + 'aleph_state.csv', self.aleph_state_step_list, delimiter=',')
-            np.savetxt(sim_dir_path + 'aleph_beta.csv', self.aleph_beta_step_list, delimiter=',')
-            np.savetxt(sim_dir_path + 'satisfy_unsatisfy_count.csv', self.satisfy_unsatisfy_count_list, delimiter=',')
 
         episode_data = self.format()
         with open(sim_dir_path + f'episode{self.epi}_{uuid.uuid4().hex[:6]}.pickle', 'wb') as f:
