4834f1bf2e8e105dc93d9de2f7c88292a43d8ac7
Jun <kumejun2000@gmail.com>
2025-01-25 15:05:00 +0900

    MountainCarRewardWrapperを使用するよう追加

--- utils/create_env_utils.py
+++ utils/create_env_utils.py
@@ -2,6 +2,8 @@ import gymnasium as gym
 from gymnasium.wrappers import AtariPreprocessing, FrameStackObservation
 import ale_py
 
+from wrapper.mountain_car_reward_wrapper import MountainCarRewardWrapper
+
 
 def create_env(env_name, algo):
     # TODO: Atariアルゴリズムを採用，Convを削除，のちにAtari→Convに変更
@@ -11,4 +13,6 @@ def create_env(env_name, algo):
         env = FrameStackObservation(env, stack_size=4)
     else:
         env = gym.make(env_name, render_mode='rgb_array')
+        if 'MountainCar' in env_name:
+            env = MountainCarRewardWrapper(env)
     return env
