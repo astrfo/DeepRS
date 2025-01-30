b2ec4133b88191f32f77485a5af3f341f6d09dd1
Jun <kumejun2000@gmail.com>
2025-01-30 23:09:31 +0900

    DQNでBasePolicyを継承

--- policy/dqn.py
+++ policy/dqn.py
@@ -1,75 +1,13 @@
 import numpy as np
 import torch
-import torch.nn as nn
-import torch.optim as optim
 
-from memory.replay_buffer import ReplayBuffer
-from network.qnet import QNet
+from policy.base_policy import BasePolicy
 
 
-class DQN:
-    def __init__(self, model=QNet, **kwargs):
-        self.gamma = kwargs['gamma']
+class DQN(BasePolicy):
+    def __init__(self, model_class, **kwargs):
+        super().__init__(model_class, **kwargs)
         self.epsilon_fixed = kwargs['epsilon_fixed']
-        self.optimizer_name = kwargs['optimizer']
-        self.adam_learning_rate = kwargs['adam_learning_rate']
-        self.rmsprop_learning_rate = kwargs['rmsprop_learning_rate']
-        self.rmsprop_alpha = kwargs['rmsprop_alpha']
-        self.rmsprop_eps = kwargs['rmsprop_eps']
-        self.criterion_name = kwargs['criterion']
-        self.mseloss_reduction = kwargs['mseloss_reduction']
-        self.replay_buffer_capacity = kwargs['replay_buffer_capacity']
-        self.hidden_size = kwargs['hidden_size']
-        self.sync_model_update = kwargs['sync_model_update']
-        self.warmup = kwargs['warmup']
-        self.tau = kwargs['tau']
-        self.batch_size = kwargs['batch_size']
-        self.target_update_freq = kwargs['target_update_freq']
-        self.state_space = kwargs['state_space']
-        self.action_space = kwargs['action_space']
-        self.replay_buffer = ReplayBuffer(self.replay_buffer_capacity, self.batch_size)
-        self.device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
-        self.model_class = model
-        self.model = None
-        self.model_target = None
-        self.total_steps = None
-        self.loss = None
-
-    def reset(self):
-        self.replay_buffer.reset()
-        self.model = self.model_class(input_size=self.state_space, hidden_size=self.hidden_size, output_size=self.action_space)
-        self.model.to(self.device)
-        self.model_target = self.model_class(input_size=self.state_space, hidden_size=self.hidden_size, output_size=self.action_space)
-        self.model_target.to(self.device)
-        self.total_steps = 0
-        
-        if self.optimizer_name == 'adam':
-            self.optimizer = optim.Adam(self.model.parameters(), lr=self.adam_learning_rate)
-        elif self.optimizer_name == 'rmsprop':
-            self.optimizer = optim.RMSprop(self.model.parameters(), lr=self.rmsprop_learning_rate, alpha=self.rmsprop_alpha, eps=self.rmsprop_eps)
-        else:
-            raise ValueError(f'Invalid optimizer: {self.optimizer_name}')
-
-        if self.criterion_name == 'mseloss':
-            self.criterion = nn.MSELoss(reduction=self.mseloss_reduction)
-        elif self.criterion_name == 'smoothl1loss':
-            self.criterion = nn.SmoothL1Loss()
-        else:
-            raise ValueError(f'Invalid criterion: {self.criterion_name}')
-
-    def calc_q_value(self, state):
-        s = torch.tensor(state, dtype=torch.float64).to(self.device).unsqueeze(0)
-        with torch.no_grad():
-            return self.model(s).squeeze().to('cpu').detach().numpy().copy()
-    
-    def embed(self, state):
-        s = torch.tensor(state, dtype=torch.float64).to(self.device).unsqueeze(0)
-        with torch.no_grad():
-            return self.model.embedding(s).squeeze().to('cpu').detach().numpy().copy()
-
-    def greedy_action(self, state):
-        q_value = self.calc_q_value(state)
-        return np.random.choice(np.where(q_value == max(q_value))[0])
 
     def action(self, state):
         self.total_steps += 1
@@ -112,11 +50,3 @@ class DQN:
             self.sync_model_soft()
         else:
             raise ValueError(f'Invalid sync_model_update: {self.sync_model_update}')
-    
-    def sync_model_hard(self):
-        self.model_target.load_state_dict(self.model.state_dict())
-
-    def sync_model_soft(self):
-        with torch.no_grad():
-            for target_param, local_param in zip(self.model_target.parameters(), self.model.parameters()):
-                target_param.data.copy_(self.tau*local_param.data + (np.float64(1.0)-self.tau)*target_param.data)
