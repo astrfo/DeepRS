3b2ca3400851f43e4e1738b56f6881e0acb2f33a
Jun <kumejun2000@gmail.com>
2025-02-01 18:07:07 +0900

    不要policyの記載削除

--- utils/algo_class_utils.py
+++ utils/algo_class_utils.py
@@ -4,14 +4,7 @@ from policy.ps import PS
 from policy.rsrsaleph_q_eps_ras_choice_dqn import RSRSAlephQEpsRASChoiceDQN
 from policy.rsrsaleph_q_eps_ras_choice_centroid_dqn import RSRSAlephQEpsRASChoiceCentroidDQN
 from policy.rsrsaleph_q_eps_ras_choice_centroid_grcw_dqn import RSRSAlephQEpsRASChoiceCentroidGRCwDQN
-from policy.conv_dqn import ConvDQN
-from policy.conv_ddqn import ConvDDQN
-from policy.conv_dqn_rnd import ConvDQN_RND
 from policy.conv_dqn_atari import ConvDQNAtari
-from policy.conv_rsrs_dqn import ConvRSRSDQN
-from policy.conv_rsrsdyn_dqn import ConvRSRSDynDQN
-from policy.conv_rsrsaleph_dqn import ConvRSRSAlephDQN
-from policy.conv_rsrsaleph_q_eps_ras_choice_dqn_rnd import ConvRSRSAlephQEpsRASChoiceDQN_RND
 from policy.conv_rsrsaleph_q_eps_ras_choice_dqn_atari import ConvRSRSAlephQEpsRASChoiceDQNAtari
 from policy.conv_rsrsaleph_q_eps_ras_choice_centroid_dqn_atari import ConvRSRSAlephQEpsRASChoiceCentroidDQNAtari
 
@@ -32,14 +25,7 @@ ALGO_CLASS = {
     'RSRSAlephQEpsRASChoiceDQN': (RSRSNet, RSRSAlephQEpsRASChoiceDQN),
     'RSRSAlephQEpsRASChoiceCentroidDQN': (RSRSDQNNet, RSRSAlephQEpsRASChoiceCentroidDQN),
     'RSRSAlephQEpsRASChoiceCentroidGRCwDQN': (RSRSDQNNet, RSRSAlephQEpsRASChoiceCentroidGRCwDQN),
-    'ConvDQN': (ConvQNet, ConvDQN),
-    'ConvDDQN': (ConvQNet, ConvDDQN),
-    'ConvDQN_RND': (ConvQNet, ConvDQN_RND),
     'ConvDQNAtari': (ConvQAtariNet, ConvDQNAtari),
-    'ConvRSRSDQN': (ConvRSRSNet, ConvRSRSDQN),
-    'ConvRSRSDynDQN': (ConvRSRSNet, ConvRSRSDynDQN),
-    'ConvRSRSAlephDQN': (ConvRSRSAlephNet, ConvRSRSAlephDQN),
-    'ConvRSRSAlephQEpsRASChoiceDQN_RND': (ConvRSRSNet, ConvRSRSAlephQEpsRASChoiceDQN_RND),
     'ConvRSRSAlephQEpsRASChoiceDQNAtari': (ConvRSRSAtariNet, ConvRSRSAlephQEpsRASChoiceDQNAtari),
     'ConvRSRSAlephQEpsRASChoiceCentroidDQNAtari': (ConvRSRSAtariNet, ConvRSRSAlephQEpsRASChoiceCentroidDQNAtari)
 }
