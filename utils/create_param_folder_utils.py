ff6f8d0d611d9c3d1e2a0486b8470d86a68492f1
Jun <kumejun2000@gmail.com>
2025-02-01 10:11:34 +0900

    trimming_trailing_zeroを導入

--- utils/create_param_folder_utils.py
+++ utils/create_param_folder_utils.py
@@ -1,6 +1,7 @@
 import os
 from utils.create_hyperparameter_list_utils import create_hyperparameter_list
 from utils.create_folder_utils import create_result_dir_path_folder
+from utils.trimming_trailing_zero import trimming_trailing_zero
 from utils.default_param_utils import DEFAULT_PARAMS
 
 
@@ -14,7 +15,8 @@ def create_compare_base_param_folder(env_name, algo, ex_param):
     folder_name = algo
     for base_param_key, base_param_value in base_param.items():
         if ex_param[base_param_key] != base_param_value:
-            folder_name += f'_{base_param_key}{ex_param[base_param_key]}'
+            ex_param_base_param_value = trimming_trailing_zero(ex_param[base_param_key])
+            folder_name += f'_{base_param_key}{ex_param_base_param_value}'
     result_dir_path = create_result_dir_path_folder(env_name, folder_name)
     return result_dir_path
 
