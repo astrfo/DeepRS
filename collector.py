d57a312c9e8fb06be7652d5542aa58d1b193f479
Jun <kumejun2000@gmail.com>
2025-02-16 11:42:28 +0900

    順番の整理

--- collector.py
+++ collector.py
@@ -26,12 +26,14 @@ class Collector:
         self.reward_greedy_step_list = []
         self.survived_step_step_list = []
         self.survived_step_greedy_step_list = []
+        self.state_step_list = []
+        self.action_step_list = []
         self.q_value_step_list = []
+        self.selected_q_value_step_list = []
         self.embed_step_list = []
         self.loss_step_list = []
         self.pi_step_list = []
         self.terminal_state_count_step_list = []
-        self.state_step_list = []
 
         # episode data
         self.reward_epi_list = []
@@ -40,33 +42,21 @@ class Collector:
         self.survived_step_greedy_epi_list = []
         self.terminal_state_ratio_epi_list = []
 
-    def format(self):
-        data = {}
-        data['param'] = self.param
-        data['reward'] = self.reward_epi_list
-        data['reward_greedy'] = self.reward_greedy_epi_list
-        data['survived_step'] = self.survived_step_epi_list
-        data['survived_step_greedy'] = self.survived_step_greedy_epi_list
-        data['q_value'] = self.q_value_step_list
-        data['embed'] = self.embed_step_list
-        data['loss'] = self.loss_step_list
-        data['pi'] = self.pi_step_list
-        return data
-
     def initialize(self, sim):
         self.sim_dir_path = create_sim_folder(self.result_dir_path, sim)
         self.reward_step_list = []
         self.reward_greedy_step_list = []
         self.survived_step_step_list = []
         self.survived_step_greedy_step_list = []
+        self.state_step_list = []
+        self.action_step_list = []
         self.q_value_step_list = []
         self.selected_q_value_step_list = []
         self.embed_step_list = []
-        self.action_step_list = []
         self.loss_step_list = []
         self.pi_step_list = []
         self.terminal_state_count_step_list = []
-        self.state_step_list = []
+
         self.reward_epi_list = []
         self.reward_greedy_epi_list = []
         self.survived_step_epi_list = []
@@ -74,6 +64,7 @@ class Collector:
         self.terminal_state_ratio_epi_list = []
 
     def reset(self):
+        # episode data で使用する step data を初期化
         self.reward_step_list = []
         self.reward_greedy_step_list = []
         self.survived_step_step_list = []
@@ -83,14 +74,13 @@ class Collector:
     def collect_step_data(self, reward, survived_step):
         self.reward_step_list.append(reward)
         self.survived_step_step_list.append(survived_step)
+        self.state_step_list.append(self.agent.current_state)  # CartPole の場合は state を保存するが，他の環境では保存しない
+        self.action_step_list.append(self.agent.current_action)
         q_value = self.policy.calc_q_value(self.agent.current_state)
         self.q_value_step_list.append(q_value)
         self.selected_q_value_step_list.append(q_value[self.agent.current_action])
-        self.action_step_list.append(self.agent.current_action)
-        self.state_step_list.append(self.agent.current_state)
         if hasattr(self.policy, 'embed') and self.policy.embed is not None:
-            embed = self.policy.embed(self.agent.current_state)
-            self.embed_step_list.append(embed)
+            self.embed_step_list.append(self.policy.embed(self.agent.current_state))
         if hasattr(self.policy, 'loss') and self.policy.loss is not None:
             self.loss_step_list.append(self.policy.loss.item())
         if hasattr(self.policy, 'pi') and self.policy.pi is not None:
@@ -117,16 +107,14 @@ class Collector:
         np.savetxt(self.sim_dir_path + f'reward_greedy_epi{epi}.csv', self.reward_greedy_epi_list, delimiter=',')
         np.savetxt(self.sim_dir_path + f'survived_step_epi{epi}.csv', self.survived_step_epi_list, delimiter=',')
         np.savetxt(self.sim_dir_path + f'survived_step_greedy_epi{epi}.csv', self.survived_step_greedy_epi_list, delimiter=',')
+        np.savetxt(self.sim_dir_path + f'state_epi{epi}.csv', self.state_step_list, delimiter=',')
+        np.savetxt(self.sim_dir_path + f'action_epi{epi}.csv', self.action_step_list, delimiter=',')
         np.savetxt(self.sim_dir_path + f'q_value_epi{epi}.csv', self.q_value_step_list, delimiter=',')
         np.savetxt(self.sim_dir_path + f'selected_q_value_epi{epi}.csv', self.selected_q_value_step_list, delimiter=',')
         np.savetxt(self.sim_dir_path + f'embed_epi{epi}.csv', self.embed_step_list, delimiter=',')
-        np.savetxt(self.sim_dir_path + f'action_epi{epi}.csv', self.action_step_list, delimiter=',')
         np.savetxt(self.sim_dir_path + f'loss_epi{epi}.csv', self.loss_step_list, delimiter=',')
         np.savetxt(self.sim_dir_path + f'pi_epi{epi}.csv', self.pi_step_list, delimiter=',')
         np.savetxt(self.sim_dir_path + f'terminal_state_ratio_epi{epi}.csv', self.terminal_state_ratio_epi_list, delimiter=',')
-        np.savetxt(self.sim_dir_path + f'state_epi{epi}.csv', self.state_step_list, delimiter=',')
-        save_epi1000_plot(self, epi)
-        
         episode_data = self.format()
         with open(self.sim_dir_path + f'episode{epi}_{uuid.uuid4().hex[:6]}.pickle', 'wb') as f:
             pkl.dump(episode_data, f)
@@ -137,16 +125,14 @@ class Collector:
         np.savetxt(self.sim_dir_path + 'reward_greedy.csv', self.reward_greedy_epi_list, delimiter=',')
         np.savetxt(self.sim_dir_path + 'survived_step.csv', self.survived_step_epi_list, delimiter=',')
         np.savetxt(self.sim_dir_path + 'survived_step_greedy.csv', self.survived_step_greedy_epi_list, delimiter=',')
+        np.savetxt(self.sim_dir_path + 'state.csv', self.state_step_list, delimiter=',')
+        np.savetxt(self.sim_dir_path + 'action.csv', self.action_step_list, delimiter=',')
         np.savetxt(self.sim_dir_path + 'q_value.csv', self.q_value_step_list, delimiter=',')
         np.savetxt(self.sim_dir_path + 'selected_q_value.csv', self.selected_q_value_step_list, delimiter=',')
         np.savetxt(self.sim_dir_path + 'embed.csv', self.embed_step_list, delimiter=',')
-        np.savetxt(self.sim_dir_path + 'action.csv', self.action_step_list, delimiter=',')
         np.savetxt(self.sim_dir_path + 'loss.csv', self.loss_step_list, delimiter=',')
         np.savetxt(self.sim_dir_path + 'pi.csv', self.pi_step_list, delimiter=',')
         np.savetxt(self.sim_dir_path + 'terminal_state_ratio.csv', self.terminal_state_ratio_epi_list, delimiter=',')
-        np.savetxt(self.sim_dir_path + 'state.csv', self.state_step_list, delimiter=',')
-        save_episode_plot(self)
-
         episode_data = self.format()
         with open(self.sim_dir_path + f'episode{self.epi}_{uuid.uuid4().hex[:6]}.pickle', 'wb') as f:
             pkl.dump(episode_data, f)
